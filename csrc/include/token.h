8960c568099d5bcc19119d25c973ae9868904dff
ijpq <509634578tk@gmail.com>
2026-03-21 21:33:25 +0800

    fix number output

--- csrc/include/token.h
+++ csrc/include/token.h
@@ -9,9 +9,9 @@ class Token;
 
 using Literal = std::variant<double, std::string_view, std::monostate>;
 
-std::string tokentype_to_string(TokenType token);
 std::ostream &operator<<(std::ostream &os, const Token &token);
-std::string literal_to_str(Literal literal);  // 修复:返回 std::string
+std::string literal_to_str(Literal literal);
+
 class Token {
   TokenType type;
   std::string_view lexeme;
@@ -22,9 +22,9 @@ class Token {
 public:
   Token(TokenType _type, std::string_view _lexeme, Literal _literal, int _line)
       : type(_type), lexeme(_lexeme), literal(_literal), line(_line) {
-    output_str = tokentype_to_string(type) + " " + 
+    output_str = tokenTypeToString(type) + " " + 
                  std::string(lexeme) + " " +
-                 literal_to_str(literal);  // 现在返回 std::string,安全
+                 literal_to_str(literal);
   }
 
   std::string_view to_str() const { return output_str; }
--- csrc/token.cpp
+++ csrc/token.cpp
@@ -1,5 +1,30 @@
 #include "include/token.h"
+#include <sstream>
+#include <iomanip>
+#include <cmath>
+
 namespace Lexeme {
+
+std::string double_to_string(double value) {
+  if (std::floor(value) == value && std::isfinite(value)) {
+    std::ostringstream oss;
+    oss << std::fixed << std::setprecision(1) << value;
+    return oss.str();
+  }
+  
+  std::ostringstream oss;
+  oss << value;
+  std::string result = oss.str();
+  
+  if (result.find('.') != std::string::npos) {
+    while (result.length() > 1 && result.back() == '0' && 
+           result[result.length() - 2] != '.') {
+      result.pop_back();
+    }
+  }
+  
+  return result;
+}
 
 std::string literal_to_str(Literal literal) {
   return std::visit(
@@ -8,38 +33,13 @@ std::string literal_to_str(Literal literal) {
         if constexpr (std::is_same_v<T, std::monostate>)
           return "null";
         else if constexpr (std::is_same_v<T, double>)
-          return std::to_string(args);
+          return double_to_string(args);
         else if constexpr (std::is_same_v<T, std::string_view>)
           return std::string(args);
       },
       literal);
 }
-std::string tokentype_to_string(TokenType type) {
-    static const std::string strings[] = {
-        "LEFT_PAREN", "RIGHT_PAREN", "LEFT_BRACE", "RIGHT_BRACE",
-        "COMMA", "DOT", "MINUS", "PLUS", "SEMICOLON", "SLASH", "STAR",
 
-        "BANG", "BANG_EQUAL",
-        "EQUAL", "EQUAL_EQUAL",
-        "GREATER", "GREATER_EQUAL",
-        "LESS", "LESS_EQUAL",
-
-        "IDENTIFIER", "STRING", "NUMBER",
-
-        "AND", "CLASS", "ELSE", "FALSE", "FUN", "FOR", "IF", "NIL", "OR",
-        "PRINT", "RETURN", "SUPER", "THIS", "TRUE", "VAR", "WHILE",
-
-        "EOF"
-    };
-
-    int index = static_cast<int>(type);
-    
-    if (index < 0 || index >= (sizeof(strings) / sizeof(strings[0]))) {
-        return "UNKNOWN_TOKEN";
-    }
-
-    return strings[index];
-}
 std::ostream &operator<<(std::ostream &os, const Token &token) {
   os << token.to_str();
   return os;
